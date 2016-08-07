3418300a9cc17ffa862a33443123e21a7a5a24e2
Dennis Tusk <dennis.tusk@getresponse.com>
2016-08-07 21:12:41 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict';
+const PORT  = process.env.PORT || 9100;
+var connect = require('connect');
+
+connect()
+.use(connect.static(__dirname + '/dist'))
+.listen(PORT);
+
+console.log('Server is running on port: ' + PORT);
